450880991c560b6049a99269d46866b08ce9f10f
shez222 <bilal.shehroz420@gmail.com>
2024-10-15 08:10:27 +0500

    cha

--- models/index.js
+++ models/index.js
@@ -1,20 +1,17 @@
 const Sequelize = require('sequelize');
-const config = require('../config/config.json');
 const fs = require('fs');
 const path = require('path');
 
-const env = process.env.NODE_ENV || 'development';
-const configEnv = config[env];
+// Use a DATABASE_URL from environment variables, or fallback to a default if not set
+const databaseURI = process.env.MYSQL_ADDON_URI || 'your_database_uri_here';
 
-const sequelize = new Sequelize(configEnv.database, configEnv.username, configEnv.password, {
-  host: configEnv.host,
-  port:configEnv.port,
-  dialect: configEnv.dialect,
-  logging: false
+const sequelize = new Sequelize('mysql://uswwm1bmvndme5uy:NNQbj1cLNn6gCdXSSTf4@ba0rsplolhaibg5cm7jm-mysql.services.clever-cloud.com:3306/ba0rsplolhaibg5cm7jm', {
+  logging: false, // Set this to `true` if you want to see SQL queries in the console
 });
 
 const db = {};
 
+// Dynamically import all models from the current directory
 fs.readdirSync(__dirname)
   .filter(file => file !== 'index.js')
   .forEach(file => {
@@ -22,6 +19,7 @@ fs.readdirSync(__dirname)
     db[model.name] = model;
   });
 
+// Handle model associations if any
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
@@ -32,6 +30,44 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
+
+
+
+// const Sequelize = require('sequelize');
+// const config = require('../config/config.json');
+// const fs = require('fs');
+// const path = require('path');
+
+// const env = process.env.NODE_ENV || 'development';
+// const configEnv = config[env];
+
+// const sequelize = new Sequelize(configEnv.database, configEnv.username, configEnv.password, {
+//   host: configEnv.host,
+//   port:configEnv.port,
+//   dialect: configEnv.dialect,
+//   logging: false
+// });
+
+// const db = {};
+
+// fs.readdirSync(__dirname)
+//   .filter(file => file !== 'index.js')
+//   .forEach(file => {
+//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+//     db[model.name] = model;
+//   });
+
+// Object.keys(db).forEach(modelName => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+// module.exports = db;
 
 
 // const Sequelize = require('sequelize');
